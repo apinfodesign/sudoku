2e14ca66f4a71f033d89fc29a46cd158793528ea
Russell DuBrock <russell.dubrock@gmail.com>
2015-02-18 17:09:46 -0800

    block field added to data object and value field changed to integer

--- databuilder.js
+++ databuilder.js
@@ -2,18 +2,61 @@ var boardString = process.argv[2];
 
 var boardArray = boardString.split('');
 
+var xFinder = function(i, index){
+  return (i+1) - (index * 10) + index;
+}
+
+var blockPopulator = function (index, xFinder) {
+  var leftBoxX = xFinder < 4;
+  var midBoxX = xFinder > 3 && xFinder < 7;
+  var rightBoxX = xFinder > 6 && xFinder < 10;
+  var topBoxY = index < 4;
+  var midBoxY = index > 3 && index < 7;
+  var botBoxY = index > 6 && index < 10;
+
+
+  if(topBoxY && leftBoxX) {
+    return 1;
+  }
+  if(midBoxX && topBoxY) {
+    return 2;
+  } 
+  if(rightBoxX && topBoxY) {
+    return 3;
+  } 
+  if(leftBoxX && midBoxY){
+    return 4;
+  }
+  if(midBoxX && midBoxY){
+    return 5;
+  }
+  if(rightBoxX && midBoxY){
+    return 6;
+  }
+  if(leftBoxX && botBoxY){
+    return 7;
+  }
+  if(midBoxX && botBoxY){
+    return 8;
+  }
+  if(rightBoxX && botBoxY) {
+    return 9;
+  };
+}
 
 function dataBuilder(array) {
   var boardData = {};
   var lines = [0,9,18,27,36,45,54,63,72];
   lines.forEach(function(value, index){
     for (var i = value; i < value+9; i++) {
-      boardData[i+1] = {value: array[i], x: (i+1) - (index * 10) + index, y: (index + 1), relations: relations};
-    };
+      if(array[i] === ' '){
+        boardData[i+1] = {value: null, x: xFinder(i, index), y: (index + 1), block: blockPopulator((index + 1), xFinder(i, index))};
+      } else {
+        boardData[i+1] = {value: parseInt(array[i]), x: xFinder(i, index), y: (index + 1), block: blockPopulator(index+1, xFinder(i, index))};}
+      };
 
   })
   console.log(boardData);
-  relations(boardData);
 }
 
 // Possible relations addition
@@ -23,7 +66,6 @@ function dataBuilder(array) {
 
 //     }
 // }
-
 
 dataBuilder(boardArray);
 
