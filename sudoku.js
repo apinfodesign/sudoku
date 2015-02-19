64db57044840f7493893ad9b240f0eed4afe3223
Russell DuBrock <russell.dubrock@gmail.com>
2015-02-19 14:46:17 -0800

    merge commit

--- sudoku.js
+++ sudoku.js
@@ -1,129 +1,25 @@
  
-//how to read the object
-var read = sudokuObject['81'].x ;
-console.log("sudokuObject is " + sudokuObject);	
-console.log("sudokuObject read is " + read ) ;	
 
-//how to read the sudArray
-console.log("sudArray is " + sudArray[1][1].x);
- 
-var databuilder = require('./databuilder.js')
+var databuilder = require('./arraybuilder.js')
 var printer = require('./boardprinter.js')
 var boardString = process.argv[2];
 var boardArray = boardString.split('');
  
-  
-function findPossibles(sudokuObject) {
-	var countnull=0;  //testing only
-	var row, column, squareNum;
-	for (var key in sudokuObject) {
-		var possibles = [];
-		var obj = sudokuObject[key];
-		var x = obj.x;
-		var y = obj.y;
+databuilder(boardArray, findOptions);
 
-
-		//if current sequare has value NULL
-		if(obj.value === null){ 
-
-			countnull= countnull + 1;
-			// console.log("found this many nulls:" + countnull);
-
-			//iterate over all possible values for the empty square
-			for (var intVal = 1; intVal <= 9; intVal++){
-				var element=intVal;   
-				//pass intVal of location x.y to matchRow
-				matchRow(x, y, element);
-				//matchColumn(x, y, element);
-				
-				if (matchRow == false && matchColumn == false){
-					possibles.push();
-				}
-			}
-		}	
-	}
-};	
-
-//matchRow checks 9 squares in row for match with element
-// x and y = where    element = what we are looking for 
-function matchRow(x, y, element) {
-	var testcount=0;
-
-
-
-
-	// iterate over all X for this Y row
-	for (i = 1; i <= 9; i++){	      
-	//check 81 locations to find 8 locations for comparison 
-		for (i=0; i<80; i++){
-			var index = i+1;
-			// console.log(sudArray[index].y + " is sudArray[index].y");
-			// console.log(sudArray[index].value + " is sudArray[index].value");
-			if 	(   
-						(sudArray[index].x 	=== x )
- 					
- 					&& ( sudArray[index].y 	=== y )  
-
-				    && ( element        	=== sudArray.value) 
-			    )
-				{ testcount = testcount+1;
-				  console.log("testcount is " + testcount);
-				  return true;
-				}
- 			//console.log(element + " is the element");
+function findOptions (object) {
+		var row, col;
+	for (var row = 0; row < 8; row++) {
+		for (var col = 0; col < 8; col++) {
+			var box = object[row][col].box;
+			console.log(box);
+			// matchRows(row, col);
+			// matchColumns(row, col);
+			// matchBoxes();
 		}
-	}
-};
- 
+	};
+}  
 
-// function findPossibles(sudokuObject) {
-// 	var row, column, squareNum;
-// 	for (var key in sudokuObject) {
-// 		var possibles = [];
-// 		var obj = sudokuObject[key];
-// 		var x = obj.x;
-// 		var y = obj.y;
-
-// 		if(obj.value === null){ 
-// 			for (var intVal = 1; intVal <= 9; intVal++){
-// 				matchRow(x, y, element);
-// 				//matchColumn(x, y, element);
-				
-// 				if (matchRow == false && matchColumn == false){
-// 					possibles.push();
-// 				}
-// 			}
-
-// 		}	
-// 	}
-// };	
-
-// function matchRow(x, y, element) {
-// 	for (i = 1; i <= 9; i++){
-// 		//extract the value of (x,i)
-// 		if(element === sudokuObject[]) {
-// 			console.log(element);
-// 	}
-// 	}
+// function matchRows () {
+// 	if 
 // }
-
-databuilder(boardArray);
-printer();
- 
-// //		var y = key.y;/
-// 	//	console.log('x,y: ',x+' , '+y);
-
-// 		//rows
-// 		
-// 			//look at the value of x,r
-// 			///console.log('r: ',r);
-// 			for (var y = 1; y <= 9; y) {
-// 			//look at the value of col, y
-// 			//console.log('c: ',y);
-// 			}
-// 		}  
-// 		//columns
-// 	}
-// }
-
-findPossibles(sudokuObject);
