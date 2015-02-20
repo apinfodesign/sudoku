fb1949fadf2c52529380e576c5b66aab983c0fc4
Miles Hochstein <miles.hochstein@gmail.com>
2015-02-20 15:26:21 -0800

    various ideas I had to control state of matrix

--- sudoku.js
+++ sudoku.js
@@ -10,9 +10,9 @@ var boardString = "8  2    5  97    4 25 1  9 2    7   96 3 1 52   6    1 8  4 7
 var boardArray = boardString.split('');
  
  var cycleCount = 0;
-var cycleCountLimit = 20;
+var cycleCountLimit = 200;
 var masterCycleCount = 0;
-var masterCycleCountLimit = 200;
+var masterCycleCountLimit = 1000;
 var arrayDepot = [];
 var arrayDepotIndex = 0;
 
@@ -56,7 +56,6 @@ function findOptions(fullArray){
 		console.log('array depot length ' + arrayDepot.length)
 		//insertSingletonValues(fullArray);
  		insertSingletonValue(fullArray);   //insert ONLY ONE value
- 
 };
 
 //Inserts only one singleton value prior to recalculation
@@ -73,99 +72,85 @@ function insertSingletonValue(fullArray){
 		var arrayToPass = arrayDepotIndex - 1;
 		buildPrinterString(arrayDepot[arrayToPass]);
 		findOptions(arrayDepot[arrayToPass]);
-		return;
-		
-		
+		return;	
 	};
 	
 	for (row=0; row<9; row++) {
 		for (col=0; col<9; col++) {
 			if (fullArray[row][col].possibles.length === 1 && fullArray[row][col].value === null) {
+
 				possibleLengths = 1;
-				fullArray[row][col].value = fullArray[row][col].possibles[0];
+				
+				fullArray[row][col].value = fullArray[row][col].possibles[0];  //assign the value!
+				
+
 				console.log('Singleton assigned at: ' + (col + 1) + ', ' + (9-row));
 				console.log('Block is: ' + fullArray[row][col].block);
 				console.log('Possibles Array for location is ' + fullArray[row][col].possibles[0]);
 				buildPrinterString(fullArray);
-
-
-				//after finding ONE singleton, display something
-				if (possibleLengths === 0) {
-					console.log('NO NEW POSSIBLES ARRAYS WITH A LENGTH OF 1');
-					buildPrinterString(fullArray);
-					insertDupleValues(fullArray);
-				}
 
 				//after finding ONE singleton, find new options
 				console.log("Calling findOptions after one singleton possibles added.")
 				findOptions(fullArray);
-
-
 			}
-		//solve for possibles array length of 2	
-		else if (fullArray[row][col].possibles.length === 2 && fullArray[row][col].value === null) {
-			
-			//insert the first value of the 2 possibles
-			fullArray[row][col].value = fullArray[row][col].possibles[0];
-				console.log('Possibles=Two - Solving for Position Zero '); 
-				console.log('Singleton assigned at: ' + (col + 1) + ', ' + (9-row));
-				console.log('Block is: ' + fullArray[row][col].block);
-				console.log('Possibles Array for location is ' + fullArray[row][col].possibles[0] + 
-								" and " + fullArray[row][col].possibles[1] );
-				buildPrinterString(fullArray);
-
-				console.log("Calling findOptions after one singleton possibles added.")
+			else if (fullArray[row][col].possibles.length > 1 && fullArray[row][col].value === null) 
+			{
+				
+				//assign value and shorten array on left side
+				fullArray[row][col].value =  fullArray[row][col].possibles.shift() ;  
 				findOptions(fullArray);
 			}
 
-
 		}
 	}
 
-
-};
-
-
-//inserts all singleton values prior to recalculation?
-function insertSingletonValues(fullArray){
-	var possibleLengths = 0;
-	var row, col;
-	cycleCount = cycleCount +1;
-	
-	if (cycleCount === cycleCountLimit) {
-		cycleCount = 0;
-		console.log('calling findOptions from arrayDepot')
-
-		arrayDepotIndex = arrayDepotIndex + 1;
-		var arrayToPass = arrayDepotIndex - 1;
-		buildPrinterString(arrayDepot[arrayToPass]);
-		findOptions(arrayDepot[arrayToPass]);
-		return;
-		
-		
-	};
-	
-	for (row=0; row<9; row++) {
-		for (col=0; col<9; col++) {
-			if (fullArray[row][col].possibles.length === 1 && fullArray[row][col].value === null) {
-				possibleLengths = 1;
-				fullArray[row][col].value = fullArray[row][col].possibles[0];
-				console.log('Singleton assigned at: ' + (col + 1) + ', ' + (9-row));
-				console.log('Block is: ' + fullArray[row][col].block);
-				console.log('Possibles Array for location is ' + fullArray[row][col].possibles[0]);
-				buildPrinterString(fullArray);
-			}
-		}
-	}
+	//after finding ONE singleton, display something
 	if (possibleLengths === 0) {
 		console.log('NO NEW POSSIBLES ARRAYS WITH A LENGTH OF 1');
 		buildPrinterString(fullArray);
 		insertDupleValues(fullArray);
 	}
-	findOptions(fullArray);
 };
 
 
+//inserts all singleton values prior to recalculation?
+// function insertSingletonValues(fullArray){
+// 	var possibleLengths = 0;
+// 	var row, col;
+// 	cycleCount = cycleCount +1;
+	
+// 	if (cycleCount === cycleCountLimit) {
+// 		cycleCount = 0;
+// 		console.log('calling findOptions from arrayDepot')
+
+// 		arrayDepotIndex = arrayDepotIndex + 1;
+// 		var arrayToPass = arrayDepotIndex - 1;
+// 		buildPrinterString(arrayDepot[arrayToPass]);
+// 		findOptions(arrayDepot[arrayToPass]);
+// 		return;
+		
+		
+// 	};
+	
+// 	for (row=0; row<9; row++) {
+// 		for (col=0; col<9; col++) {
+// 			if (fullArray[row][col].possibles.length === 1 && fullArray[row][col].value === null) {
+// 				possibleLengths = 1;
+// 				fullArray[row][col].value = fullArray[row][col].possibles[0];
+// 				console.log('Singleton assigned at: ' + (col + 1) + ', ' + (9-row));
+// 				console.log('Block is: ' + fullArray[row][col].block);
+// 				console.log('Possibles Array for location is ' + fullArray[row][col].possibles[0]);
+// 				buildPrinterString(fullArray);
+// 			}
+// 		}
+// 	}
+// 	if (possibleLengths === 0) {
+// 		console.log('NO NEW POSSIBLES ARRAYS WITH A LENGTH OF 1');
+// 		buildPrinterString(fullArray);
+// 		insertDupleValues(fullArray);
+// 	}
+// 	findOptions(fullArray);
+// };
 
 
 
@@ -173,8 +158,13 @@ function insertDupleValues(fullArray){
 	for (row=0; row<9; row++) {
 		for (col=0; col<9; col++) {
 			if (fullArray[row][col].possibles.length === 2 && fullArray[row][col].value === null) {
+			
 				fullArray[row][col].value = fullArray[row][col].possibles[1];
-				arrayDepot.push(fullArray);
+				
+
+				//arrayDepot.push(fullArray);  //mh trying approach with out this
+				
+
 				fullArray[row][col].value = fullArray[row][col].possibles[0];
 				console.log('duple assigned at ' + (col + 1) + ', ' + (9-row));
 				buildPrinterString(fullArray);
